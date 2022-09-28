c7d77d3b02e93ab5ecce0b875eee8f1c33b14f6d
John Adekoya <johnadexzy@gmail.com>
2022-09-28 12:31:19 +0100

    fix: implemented redux for state management

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
-import chatImg from '../assets/johndoe.jpeg';
+import LoveImg from '../assets/love.png';
 
 const Header = () => {
   return (
     <header className="bg-black-light border-b border-purple-500 flex text-white items-center p-3 gap-2">
-      <div className="w-12 rounded-full overflow-hidden">
-        <img src={chatImg} alt="avatar" className="w-full" />
-      </div>
-      <p>John Doe</p>
+      <img
+        src={LoveImg}
+        alt="avatar"
+        className="object-cover rounded-full w-10 h-10"
+      />
+      <p>Love Hub</p>
     </header>
   );
 };
